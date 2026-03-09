3dc47fabb50d59069b15db8a7aa830507729d57f
Pearse <pearseisthebest1@gmail.com>
2026-03-09 15:43:01 +0000

    Use plain local icon file paths

--- game.js
+++ game.js
@@ -72,14 +72,14 @@ const ZOMBIE_ATTACK_DAMAGE = 10;
 const ZOMBIE_ATTACK_COOLDOWN = 1.05;
 
 const ICON_PATHS = {
-  key: "assets/icons/Brass_Key.png?v=20260309d",
-  "green-herb": "assets/icons/green-herb.svg?v=20260309d",
-  "mixed-herbs": "assets/icons/mixed-herbs.svg?v=20260309d",
-  grenade: "assets/icons/Grenade.png?v=20260309d",
-  ammo: "assets/icons/Ammo.png?v=20260309d",
-  pistol: "assets/icons/pistol.svg?v=20260309d",
-  zombie: "assets/icons/zombie.svg?v=20260309d",
-  player: "assets/icons/player.png?v=20260309d"
+  key: "assets/icons/Brass_Key.png",
+  "green-herb": "assets/icons/green-herb.svg",
+  "mixed-herbs": "assets/icons/mixed-herbs.svg",
+  grenade: "assets/icons/Grenade.png",
+  ammo: "assets/icons/Ammo.png",
+  pistol: "assets/icons/pistol.svg",
+  zombie: "assets/icons/zombie.svg",
+  player: "assets/icons/player.png"
 };
 
 const iconImages = {};
